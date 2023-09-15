2d32fd952c07b68fe45ea381b8c948a0779cab65
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-15 22:11:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Lets go!</p>
+    </>
+  )
+}
+
+export default App
